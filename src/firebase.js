405db5d5dc1b98325f00f82a176b336e5f6d9c09
Fredes Virginia <94409602+FredesVirginia@users.noreply.github.com>
2024-02-28 20:50:57 +0000

    hui

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ4cKm38OGPKOO544Tp1qT4kPgUapflco",
+  authDomain: "next-chat-c3684.firebaseapp.com",
+  projectId: "next-chat-c3684",
+  storageBucket: "next-chat-c3684.appspot.com",
+  messagingSenderId: "170687277021",
+  appId: "1:170687277021:web:ddda1f58cf1769cdefb834",
+  measurementId: "G-KW7WY3DEGT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
