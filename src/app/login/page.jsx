f21056655b677263d19934325e6a4a3aafe3b717
Fredes Virginia <94409602+FredesVirginia@users.noreply.github.com>
2024-02-28 18:23:11 +0000

    dkfjni

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,8 +2,22 @@ import Image from 'next/image'
 
 export default function Login() {
   return (
-    <div className='text-cemter'>
-      hola. LOGIN
-      </div>
+    <div className='flex justify-center align-center mt-[60px]'>
+    <form className='w-[350px] bg-white h-30 text-gray-600 flex flex-col justify-center align-center p-10 lg:p-10 rounded-md'>
+    <h1 className='text-center text-xl mb-3'> Chat Fazt </h1>
+        <h1 className='text-center '> Iniciar Secion </h1>
+        <div className='flex flex-col space-y-1'>
+            
+            <label className='text-sm' >Correo</label>
+            <input required  className='w-[260px] border border-blue-200 ' />
+            <label className='text-sm' >Clave</label>
+            <input required  className='w-[260px] border border-blue-200 ' />
+           
+          
+            <button className=' w-[260px] bg-blue-200 py-1 hover:bg-blue-400 hover:text-white ' >Ingresar</button>
+            <p>¿No tienes cuenta? <a className='text-blue-400 underline' href='#'>Registrate Aqui</a></p>
+        </div>
+    </form>
+</div>
   )
 }
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,29 +1,33 @@
 import Image from 'next/image'
 
-export default function Register() {img
-  return (
-    <div className='flex justify-center align-center mt-[60px]'>
-      <form className='w-[400px] bg-red-300 h-30 text-white font-bold flex flex-col p-10'>
-       <h1 className='text-center'>Registro </h1>
-        <div className='flex flex-col space-y-1'>
-        <label >Nombre</label>
-       <input className='border border-blue-300 '/>
-       <label >Correo</label>
-       <input className='border border-blue-300 '/>
-       <label >Clave</label>
-       <input className='border border-blue-300 '/>
-       <label >Foto de Perfil</label>
-      <div>
-      <Image
-        src="/img/perfil.png"
-        width={50}
-        height={50}
-        alt="Picture of the author"
-        className=' rounded-full'
-      />
-      </div>
+export default function Register() {
+    return (
+        <div className='flex justify-center align-center mt-[60px]'>
+            <form className='w-[350px] bg-white h-30 text-gray-600 flex flex-col p-10 lg:p-10 rounded-md'>
+            <h1 className='text-center text-xl mb-3'> Chat Fazt </h1>
+                <h1 className='text-center '>Registro </h1>
+                <div className='flex flex-col space-y-1'>
+                    <label className='text-sm'>Nombre</label>
+                    <input required className=' w-[260px] border border-blue-200 ' />
+                    <label className='text-sm' >Correo</label>
+                    <input required  className='w-[260px] border border-blue-200 ' />
+                    <label className='text-sm' >Clave</label>
+                    <input required  className='w-[260px] border border-blue-200 ' />
+                    <label className='text-sm' >Foto de Perfil</label>
+                    <div className=' w-[260px] flex items-center space-x-2 py-2 '>
+                        <Image
+                            src="/img/perfil3.png"
+                            width={50}
+                            height={50}
+                            alt="Picture of the author"
+                            className=' rounded-full'
+                        />
+                        <input type="file" className='text-sm' />
+                    </div>
+                    <button className='bg-blue-200 py-1 hover:bg-blue-400 hover:text-white ' >Registrarse</button>
+                    <p>¿Ya tienes cuentas? <a className='text-blue-400 underline' href='#'>Inicia secion</a></p>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  )
+    )
 }
