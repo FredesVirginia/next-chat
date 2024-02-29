1d60feb673c1ed94402a90483d56a168d0e077d4
Fredes Virginia <94409602+FredesVirginia@users.noreply.github.com>
2024-02-29 05:30:29 +0000

    registro y rediccionamiento exitoso

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,24 +1,79 @@
 "use client"
 import Image from 'next/image'
+import {auth , storage , db} from "@/firebase";
+import {  createUserWithEmailAndPassword , updateProfile } from "firebase/auth";
+import { getStorage ,ref, uploadBytesResumable , getDownloadURL} from "firebase/storage"
+import toast from 'react-hot-toast';
+
+import {doc , setDoc} from "firebase/firestore";
 
 export default function Register() {
+  
 
-    const handleSubmit = (e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
-            console.log(e.target[0].value);
-    }
+        console.log(e.target[0].value);
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
+
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+        
+            const storageRef = ref(storage, `profile_images/${res.user.uid}`);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+        
+            uploadTask.on(
+              'state_changed',
+              (snapshot) => {
+                // Manejar cambios de estado durante la carga, si es necesario
+              },
+              (error) => {
+                console.error('Error durante la carga de la imagen:', error);
+                
+              },
+              async () => {
+                // La carga se completó con éxito, obtener la URL de descarga
+                const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+        
+                // Actualizar el perfil del usuario
+                await updateProfile(res.user, {
+                  displayName,
+                  photoURL: downloadURL,
+                });
+        
+                // Almacenar información adicional en Firestore
+                await setDoc(doc(db, 'users', res.user.uid), {
+                  uid: res.user.uid,
+                  displayName,
+                  email,
+                  photoURL: downloadURL,
+                });
+      
+                await setDoc(doc(db , "userChats" , res.user.uid), {});
+        
+                console.log('Registro exitoso');
+                window.location.href = '/';
+              }
+            );
+          } catch (error) {
+            console.error('Error durante la creación del usuario :', error);
+          }
+        };
+    
     return (
         <div className='flex justify-center align-center mt-[60px]'>
             <form onSubmit={handleSubmit} className='w-[350px] bg-white h-30 text-gray-600 flex flex-col p-10 lg:p-10 rounded-md'>
-            <h1 className='text-center text-xl mb-3'> Chat Fazt </h1>
+                <h1 className='text-center text-xl mb-3'> Chat Fazt </h1>
                 <h1 className='text-center '>Registro </h1>
                 <div className='flex flex-col space-y-1'>
                     <label className='text-sm'>Nombre</label>
                     <input required className=' w-[260px] border border-blue-200 ' />
                     <label className='text-sm' >Correo</label>
-                    <input required  className='w-[260px] border border-blue-200 ' />
+                    <input required className='w-[260px] border border-blue-200 ' />
                     <label className='text-sm' >Clave</label>
-                    <input required  className='w-[260px] border border-blue-200 ' />
+                    <input required className='w-[260px] border border-blue-200 ' />
                     <label className='text-sm' >Foto de Perfil</label>
                     <div className=' w-[260px] flex items-center space-x-2 py-2 '>
                         <Image
--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getAuth} from "firebase/auth";
-
-
+import {getStorage} from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCJ4cKm38OGPKOO544Tp1qT4kPgUapflco",
-  authDomain: "next-chat-c3684.firebaseapp.com",
-  projectId: "next-chat-c3684",
-  storageBucket: "next-chat-c3684.appspot.com",
-  messagingSenderId: "170687277021",
-  appId: "1:170687277021:web:ddda1f58cf1769cdefb834",
-  measurementId: "G-KW7WY3DEGT"
+  apiKey: "AIzaSyBwNQAxUwC5RLsWnkfgesEZybESBBZAFJ0",
+  authDomain: "next-chat-aeb5b.firebaseapp.com",
+  projectId: "next-chat-aeb5b",
+  storageBucket: "next-chat-aeb5b.appspot.com",
+  messagingSenderId: "224001724652",
+  appId: "1:224001724652:web:bc850fd15ce66dd0a815c9",
+  measurementId: "G-S4QYF3RJ6B"
 };
 
 // Initialize Firebase
- export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
